757ded15c575f16f4ca80572fc7778aabcfcb329
Gustavo Alcantara <gustavo.ca.geografia@gmail.com>
2022-05-11 13:43:56 -0300

    atualizacao_11-5

--- archives_geobr.R
+++ archives_geobr.R
@@ -1,1 +1,17 @@
+#listagem dos datasets do Geobr
 geobr::list_geobr()
+
+#Atribuição do dataset de Municipalidade
+x <- geobr::read_municipality()
+
+#Rename das variáveis de acordo com a BD
+x|>
+  dplyr::rename(
+    id_municipio = code_muni,
+    nome_municipio = name_muni,
+    codigo_estado = code_state,
+    sigla_uf = abbrev_state,
+    geometria = geom
+                )
+
+str(x)
