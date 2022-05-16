aaa7427c0f275461e7433dafd230058fbf99ee74
Gustavo Alcantara <gustavo.ca.geografia@gmail.com>
2022-05-16 15:44:23 -0300

    atualizacao_16-5

--- schools.R
+++ schools.R
@@ -1,6 +1,20 @@
 ##Geobr - Read_Schools
-
 geobr::read_schools() -> x
 
-x|>
-  dplyr::distinct(educational_level_others)
+#Diretório de Acordo com os dados da BD
+#Todas as demais infos estão 
+#basedosdados.org/dataset/br-inep-censo-escolar
+
+
+x|> #Seleção das variáveis
+  dplyr::select(code_school, 
+                date_update, 
+                geom
+  ) |> # Rename das Variáveis
+  dplyr::rename(id_escola = code_school,
+                ano = date_update,
+                geometria = geom
+    
+  )
+
+
