d529fe84cea0adfa04e4a94bf400a88a6c2c3ba6
Gustavo Alcantara <gustavo.ca.geografia@gmail.com>
2022-05-23 10:06:06 -0300

    atualizacao_23-5

--- schools.R
+++ schools.R
@@ -8,13 +8,21 @@ geobr::read_schools() -> x
 
 x|> #Seleção das variáveis
   dplyr::select(code_school, 
-                date_update, 
+                date_update,
+                abbrev_state,
                 geom
   ) |> # Rename das Variáveis
   dplyr::rename(id_escola = code_school,
                 ano = date_update,
+                id_sigla_uf = abbrev_state,
                 geometria = geom
-    
-  )
+  ) |>
+  dplyr::select(ano, #padrão BD
+                id_sigla_uf,
+                id_escola,
+                geometria
+  ) -> x
 
-
+#Salvando arquivo csv
+write.csv(x, 'escolas.csv',  na = " ", 
+          row.names = F, fileEncoding = "UTF-8")
